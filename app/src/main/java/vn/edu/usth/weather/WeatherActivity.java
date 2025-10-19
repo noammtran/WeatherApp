32d50c8684f34e1a0a6014b8b536e97d9397139a
Noammtruan <namth.23bi14235@usth.edu.vn>
2025-10-19 17:32:23 +0700

    Add toolbar with refresh and setting

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,8 +1,15 @@
 package vn.edu.usth.weather;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
+
 import com.google.android.material.tabs.TabLayout;
 
 public class WeatherActivity extends AppCompatActivity {
@@ -12,6 +19,9 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         ViewPager pager = findViewById(R.id.pager);
         WeatherPagerAdapter adapter = new WeatherPagerAdapter(getSupportFragmentManager(), this);
         pager.setOffscreenPageLimit(adapter.getCount() - 1);
@@ -19,5 +29,25 @@ public class WeatherActivity extends AppCompatActivity {
 
         TabLayout tabs = findViewById(R.id.tab_layout);
         tabs.setupWithViewPager(pager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_weather, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            Toast.makeText(this, R.string.refresh_message, Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, PrefActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
